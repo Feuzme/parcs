a0deef45b91c21b2d2a4d1aaaa204738c43f1988
Fyfeu Feuzme <feuzme@gmail.com>
2021-06-04 16:05:59 +0200

    final commit

--- src/main/java/fr/feuzme/spring/controllers/ParcController.java
+++ src/main/java/fr/feuzme/spring/controllers/ParcController.java
@@ -3,6 +3,7 @@ package fr.feuzme.spring.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,6 +18,7 @@ import fr.feuzme.spring.services.GenericService;
 
 @RestController
 @RequestMapping("parcs")
+@CrossOrigin
 public class ParcController {
 	@Autowired
 	private GenericService<Parc> service;
@@ -32,7 +34,7 @@ public class ParcController {
 	}
 	
 	@PostMapping()
-	public Parc save(Parc parc) {
+	public Parc save(@RequestBody Parc parc) {
 		return this.service.save(parc);
 	}
 	
--- src/main/java/fr/feuzme/spring/models/Parc.java
+++ src/main/java/fr/feuzme/spring/models/Parc.java
@@ -7,17 +7,40 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
-@Data
+
 @Entity
 public class Parc {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Integer id;
+	private Integer id;	
 	@Column(name = "nom")
 	private String nom;
 	@Column(name = "capacite")
 	private Integer capacite;
+	public Parc() {
+		// TODO Auto-generated constructor stub
+	}
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getNom() {
+		return nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	public Integer getCapacite() {
+		return capacite;
+	}
+	public void setCapacite(Integer capacite) {
+		this.capacite = capacite;
+	}
 	
 }
